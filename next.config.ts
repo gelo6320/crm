278aeb00a71a12e5293d06401f01e227c5911257
gelo6320 <olegbolonniy@gmail.com>
2025-04-19 18:12:31 +0200

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/crm',
   output: 'standalone',
   eslint: {
-    // Disabilita ESLint durante le build e i comandi dev
     ignoreDuringBuilds: true,
   },
 };
