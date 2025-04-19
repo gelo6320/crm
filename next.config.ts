d478030c0a3ebe2c776f7f9a4fa5ab1e35599d50
gelo6320 <olegbolonniy@gmail.com>
2025-04-19 19:58:06 +0200

    server

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
+// next.config.ts
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
   },
-};
+});
 
 module.exports = nextConfig;
