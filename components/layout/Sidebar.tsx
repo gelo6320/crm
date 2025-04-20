ccd69fd80ca665fc6dc0360bc6370e1a90673b04
gelo6320 <olegbolonniy@gmail.com>
2025-04-20 17:52:43 +0200

    app

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -4,7 +4,7 @@
 import { 
   BarChart3, Calendar, FileText, Bookmark, 
   Facebook, Share2, Users, Settings, LogOut,
-  X 
+  X, Globe 
 } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -39,6 +39,7 @@ export default function Sidebar({ open, setOpen, isMobile }: SidebarProps) {
     { name: "Calendario", href: "/calendar", icon: Calendar },
     { name: "Eventi Facebook", href: "/events", icon: Share2 },
     { name: "Sales Funnel", href: "/sales-funnel", icon: Users },
+    { name: "I tuoi siti", href: "/my-sites", icon: Globe },
     { name: "Impostazioni", href: "/settings", icon: Settings },
   ];
   
