b836514a78467425a5edacf77684a641ca37c4b5
gelo6320 <olegbolonniy@gmail.com>
2025-04-20 02:12:21 +0200

    server

---
+++ app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'CRM Costruzione Digitale',
+    short_name: 'CRM',
+    description: 'CRM costruzione digitale',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#000000',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/icons/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icons/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
